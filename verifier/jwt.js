5a815cb85ac1ec087e3e6217c73787599dd06706
itsmaruf <maruf@corexlab.dev>
2022-12-13 13:35:04 +0600

    jwt verified

--- verifier/jwt.js
+++ verifier/jwt.js
@@ -4,20 +4,58 @@ var router = express.Router();
 
 require("dotenv").config();
 
+const verifyJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  // console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).json({
+      status: 401,
+      message: "Unauthorized Access 1",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(401).json({
+        status: 401,
+        message: "Unauthorized Access 2",
+        error: err,
+      });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+  // next();
+};
+
 router.get("/", function (req, res, next) {
   res.send("Hello JWT!");
 });
 
-router.post("/", async function (req, res, next) {
+router.post("/", verifyJwt, async function (req, res, next) {
   const user = req.body;
 
   const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {
     expiresIn: "1h",
   });
 
-  res.send({ token });
+  const decoded = req.decoded;
+  // console.log("decoded", decoded);
 
-  console.log({ ...user, token });
+  if (decoded.email !== user.email && decoded.password !== user.password) {
+    return res.status(401).json({
+      status: 401,
+      message: "Unauthorized Access 3",
+      decoded,
+      user,
+    });
+  }
+  res.send({ token, decoded, user });
+  console.log({ token, ...decoded });
+
+  // console.log({ ...user, token });
 });
 
 module.exports = router;
